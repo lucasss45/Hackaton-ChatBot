e67b95c924de4bef2d5e94223a58f907531a58d8
lucasss45 <nota10.lucas@gmail.com>
2023-12-02 00:45:59 -0300

    Criando ChatBot v1.7
    mudando interação com o index e css

--- static/js/index.js
+++ static/js/index.js
@@ -3,70 +3,63 @@ let input = document.querySelector('#input');
 let botaoEnviar = document.querySelector('#botao-enviar');
 
 async function enviarMensagem() {
-    if(input.value == "" || input.value == null) return;
-    let mensagem = input.value;
+    if (!input.value.trim()) return;
+
+    const mensagem = input.value.trim();
     input.value = "";
 
-    let novaBolha = criaBolhaUsuario();
-    novaBolha.innerHTML = mensagem;
-    chat.appendChild(novaBolha);
+    const novaBolhaUsuario = criaBolhaUsuario();
+    novaBolhaUsuario.textContent = mensagem;
+    chat.appendChild(novaBolhaUsuario);
 
-    let novaBolhaBot = criaBolhaBot();
+    const novaBolhaBot = criaBolhaBot();
     chat.appendChild(novaBolhaBot);
     vaiParaFinalDoChat();
-    
-// Envia requisição com a mensagem para a API do ChatBot
-    const resposta = await fetch("http://127.0.0.1:5000/chat", {
-        method: "POST",
-        headers: {
-        "Content-Type": "application/json",
-        },
-        body: JSON.stringify({'msg':mensagem}),
-    });
 
-    const decodificador = new TextDecoder();
-    const leitorDaResposta = resposta.body.getReader();
-    let respostaParcial = "";
-    while (true) {
-        // Aguarda e recebe o próximo pedaço da resposta da API
-        const { done: terminou, value: pedacoDaResposta } = await leitorDaResposta.read();
-        if (terminou) break;
+    // Simulação da resposta da API localmente
+    if (window.location.hostname === "localhost") {
+        setTimeout(() => {
+            const respostaSimulada = "Resposta simulada do ChatBot para: " + mensagem;
+            const novaBolhaBotParte = criaBolhaBot();
+            novaBolhaBotParte.innerHTML = respostaSimulada;
+            chat.appendChild(novaBolhaBotParte);
+            vaiParaFinalDoChat();
+        }, 50000); // Simula um atraso de 1 segundo
+    } else {
+        // Envia requisição com a mensagem para a API do ChatBot
+        try {
+            const resposta = await fetch("http://127.0.0.1:5000/chat", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ 'msg': mensagem }),
+            });
 
-        // Concatena o novo pedaço da resposta com a resposta parcial e atualiza na tela
-        respostaParcial += decodificador.decode(pedacoDaResposta).replace(/\n/g, '<br>');
-        novaBolhaBot.innerHTML = respostaParcial;
-        vaiParaFinalDoChat();
+            const decodificador = new TextDecoder();
+            const leitorDaResposta = resposta.body.getReader();
+            let respostaParcial = "";
+
+            while (true) {
+                const { done: terminou, value: pedacoDaResposta } = await leitorDaResposta.read();
+                if (terminou) break;
+
+                respostaParcial += decodificador.decode(pedacoDaResposta).replace(/\n/g, '<br>');
+
+                const novaBolhaBotParte = criaBolhaBot();
+                novaBolhaBotParte.innerHTML = respostaParcial;
+                chat.appendChild(novaBolhaBotParte);
+            }
+
+            vaiParaFinalDoChat();
+        } catch (erro) {
+            console.error("Erro na requisição:", erro);
+        }
     }
-    
 }
 
-function criaBolhaUsuario() {
-    let bolha = document.createElement('p');
-    bolha.classList = 'chat__bolha chat__bolha--usuario';
-    return bolha;
-}
-
-function criaBolhaBot() {
-    let bolha = document.createElement('p');
-    bolha.classList = 'chat__bolha chat__bolha--bot';
-    return bolha;
-}
+// Restante do código ...
 
 function vaiParaFinalDoChat() {
     chat.scrollTop = chat.scrollHeight;
-}
-
-botaoEnviar.addEventListener('click', enviarMensagem);
-input.addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        botaoEnviar.click();
-    }
-});
-
-function limparConversa(){
-    const limpar = fetch("http://127.0.0.1:5000/limparhistorico", {
-        method: "POST"
-    });
-    chat.innerHTML = "<p class='chat__bolha chat__bolha--bot'>Olá! Eu sou o assistente virtual da EcoMart ~<br/><br/>Como posso te ajudar?</p>";
 }
