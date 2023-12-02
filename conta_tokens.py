9e02ada672cf9561395d6c81c057ed478910820e
lucasss45 <nota10.lucas@gmail.com>
2023-12-02 00:42:00 -0300

    ChatBot JS v1
    tirando o desnecessario

--- conta_tokens.py
+++
@@ -1,7 +0,0 @@
-import tiktoken
-
-def conta_tokens(prompt):
-    codificador = tiktoken.encoding_for_model("gpt-3.5-turbo")
-    lista_de_tokens = codificador.encode(prompt)
-    contagem = len(lista_de_tokens)
-    return contagem
