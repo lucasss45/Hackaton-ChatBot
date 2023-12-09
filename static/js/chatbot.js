dbff64833313b920e7f6c257f42b2faa8b02d386
lucasss45 <nota10.lucas@gmail.com>
2023-12-09 00:05:09 -0300

    atualizando Lógica
    V2

--- static/js/chatbot.js
+++ static/js/chatbot.js
@@ -13,95 +13,64 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     async function enviarMensagem() {
-        if (input.value.trim() === "") return;
+        if (!input.value.trim()) return;
 
         const mensagem = input.value.trim();
         input.value = "";
 
-        const bolhaUsuario = criaBolha("chat__bolha--usuario", mensagem);
-        chat.appendChild(bolhaUsuario);
+        const novaBolhaUsuario = criaBolhaUsuario();
+        novaBolhaUsuario.textContent = mensagem;
+        chat.appendChild(novaBolhaUsuario);
 
-        // Responde com informações específicas do tema
-        const resposta = await obterInformacoesDoTemaSimulado(mensagem.toLowerCase());
-        const bolhaBot = criaBolha("chat__bolha--bot", resposta);
-        chat.appendChild(bolhaBot);
+        // Agora, vamos extrair o resultado da Promise antes de usá-lo
+        const respostaPromise = obterInformacoesDoTemaSimulado(mensagem.toLowerCase());
+        const resposta = await respostaPromise;
+
+        const novaBolhaBot = criaBolhaBot();
+        novaBolhaBot.innerHTML = resposta;
+        chat.appendChild(novaBolhaBot);
 
         vaiParaFinalDoChat();
     }
 
-    // Função para obter informações simuladas do tema
     async function obterInformacoesDoTemaSimulado(tema) {
-        const informacoesSimuladas = {
-            'ux/ui': [
-                'UX (User Experience) refere-se à experiência do usuário ao interagir com um produto.',
-                'UI (User Interface) se concentra no design visual e interativo do produto.',
-                'Princípios de design são fundamentais para criar experiências UX/UI eficazes.'
-            ],
-            'programacao': [
-                'A programação é a arte de escrever instruções para computadores executarem tarefas.',
-                'Linguagens de programação como JavaScript, Python e Java são amplamente utilizadas.',
-                'Desenvolvimento web, mobile e software são áreas comuns de programação.'
-            ],
-            'marketing': [
-                'Marketing envolve estratégias para promover produtos ou serviços.',
-                'Marketing digital inclui publicidade online, SEO e mídias sociais.',
-                'Entender o público-alvo é crucial para campanhas de marketing eficazes.'
-            ],
-            'gestao/vendas': [
-                'Gestão envolve liderar e administrar recursos em uma organização.',
-                'Vendas incluem o processo de persuadir clientes a adquirirem produtos ou serviços.',
-                'Habilidades de comunicação e negociação são essenciais em vendas e gestão.'
-            ],
-            'nova economia': [
-                'A nova economia refere-se a modelos de negócios inovadores e disruptivos.',
-                'Tecnologias emergentes desempenham um papel crucial na nova economia.',
-                'Colaboração e agilidade são elementos-chave na nova economia.'
-            ],
-            'hub de inovação': [
-                'Hubs de inovação são espaços que promovem a colaboração e o desenvolvimento de ideias.',
-                'Empresas, startups e empreendedores geralmente se reúnem em hubs de inovação.',
-                'Esses hubs impulsionam a criatividade e a troca de conhecimento.'
-            ],
-            'tecnologia': [
-                'Tecnologia abrange o estudo, desenvolvimento e aplicação de ferramentas e sistemas.',
-                'Avanços tecnológicos transformam rapidamente a sociedade e os negócios.',
-                'Setores como inteligência artificial, blockchain e IoT estão na vanguarda da tecnologia.'
-            ],
-            'institutos de tecnologia': [
-                'Institutos de tecnologia são instituições de ensino focadas em disciplinas tecnológicas e científicas.',
-                'Essas instituições oferecem programas de graduação e pesquisa em áreas como engenharia e ciência da computação.',
-                'Colaborações entre empresas e institutos de tecnologia impulsionam inovações.'
-            ]
-        };
+        // Carrega o JSON simulado localmente
+        const dadosSimulados = await carregarJSONLocalmente("dadosChatbot.json");
 
-        const informacoes = informacoesSimuladas[tema];
+        // Busca informações no JSON
+        const informacoes = dadosSimulados.temas[tema];
 
         if (informacoes) {
             return informacoes.join('<br>');
         } else {
-            return 'Desculpe, não tenho informações para o tema especificado.';
+            // Se o tema não existir, retorna uma resposta simulada genérica
+            const respostasSimuladas = dadosSimulados.respostas_simuladas;
+            return respostasSimuladas[Math.floor(Math.random() * respostasSimuladas.length)];
         }
     }
 
-    function criaBolha(classe, conteudo) {
-        const bolha = document.createElement('p');
-        bolha.classList = `chat__bolha ${classe}`;
-        bolha.innerHTML = conteudo;
-        return bolha;
+    function criaBolhaBot() {
+        const bolhaBot = document.createElement('p');
+        bolhaBot.classList = 'chat__bolha chat__bolha--bot';
+        return bolhaBot;
     }
 
     function vaiParaFinalDoChat() {
         chat.scrollTop = chat.scrollHeight;
     }
+
+    async function carregarJSONLocalmente(caminho) {
+        try {
+            const respostaSimuladaJSON = await fetch(caminho);
+            const dadosSimulados = await respostaSimuladaJSON.json();
+            return dadosSimulados;
+        } catch (erro) {
+            console.error("Erro ao carregar o JSON:", erro);
+            // Pode tratar o erro de outra forma se necessário
+            return {};
+        }
+    }
 });
-
-
-
-
-
-
-
-
 
 
 
@@ -170,3 +139,43 @@ document.addEventListener("DOMContentLoaded", function () {
 //         chat.innerHTML = "<p class='chat__bolha chat__bolha--bot'>Olá! Eu sou o assistente virtual da EcoMart ~<br/><br/>Como posso te ajudar?</p>";
 //     }
 // });
+
+
+ // async function enviarMensagem() {
+    //     if (input.value.trim() === "") return;
+
+    //     const mensagem = input.value.trim();
+    //     input.value = "";
+
+    //     const bolhaUsuario = criaBolha("chat__bolha--usuario", mensagem);
+    //     chat.appendChild(bolhaUsuario);
+
+    //     // Responde com informações específicas do tema
+    //     const resposta = await obterInformacoesDoTemaSimulado(mensagem.toLowerCase());
+    //     const bolhaBot = criaBolha("chat__bolha--bot", resposta);
+    //     chat.appendChild(bolhaBot);
+
+    //     vaiParaFinalDoChat();
+    // }
+
+    // async function obterInformacoesDoTemaSimulado(tema) {
+    //     const informacoesSimuladas = {
+            
+    //     };
+
+    //     const informacoes = informacoesSimuladas[tema];
+
+    //     if (informacoes) {
+    //         return informacoes.join('<br>');
+    //     } else {
+    //         return 'Desculpe, não tenho informações para o tema especificado.';
+    //     }
+    // }
+
+
+// function criaBolha(classe, conteudo) {
+    //     const bolha = document.createElement('p');
+    //     bolha.classList = `chat__bolha ${classe}`;
+    //     bolha.innerHTML = conteudo;
+    //     return bolha;
+    // }
--- static/js/index.js
+++ static/js/index.js
@@ -1,65 +1,140 @@
-let chat = document.querySelector('#chat');
-let input = document.querySelector('#input');
-let botaoEnviar = document.querySelector('#botao-enviar');
+//index.js
+document.addEventListener("DOMContentLoaded", function () {
+    const chat = document.getElementById("chat");
+    const input = document.getElementById("input");
+    const botaoEnviar = document.getElementById("botao-enviar");
 
-async function enviarMensagem() {
-    if (!input.value.trim()) return;
+    botaoEnviar.addEventListener('click', enviarMensagem);
+    input.addEventListener("keyup", function (event) {
+        event.preventDefault();
+        if (event.key === "Enter") {
+            enviarMensagem();
+        }
+    });
 
-    const mensagem = input.value.trim();
-    input.value = "";
+    async function enviarMensagem() {
+        if (!input.value.trim()) return;
 
-    const novaBolhaUsuario = criaBolhaUsuario();
-    novaBolhaUsuario.textContent = mensagem;
-    chat.appendChild(novaBolhaUsuario);
+        const mensagem = input.value.trim();
+        input.value = "";
 
-    const novaBolhaBot = criaBolhaBot();
-    chat.appendChild(novaBolhaBot);
-    vaiParaFinalDoChat();
+        const novaBolhaUsuario = criaBolhaUsuario();
+        novaBolhaUsuario.textContent = mensagem;
+        chat.appendChild(novaBolhaUsuario);
 
-    // Simulação da resposta da API localmente
-    if (window.location.hostname === "localhost") {
-        setTimeout(() => {
-            const respostaSimulada = "Resposta simulada do ChatBot para: " + mensagem;
-            const novaBolhaBotParte = criaBolhaBot();
-            novaBolhaBotParte.innerHTML = respostaSimulada;
-            chat.appendChild(novaBolhaBotParte);
-            vaiParaFinalDoChat();
-        }, 50000); // Simula um atraso de 1 segundo
-    } else {
-        // Envia requisição com a mensagem para a API do ChatBot
-        try {
-            const resposta = await fetch("http://127.0.0.1:5000/chat", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
+        const resposta = obterInformacoesDoTemaSimulado(mensagem.toLowerCase());
+        const novaBolhaBot = criaBolhaBot();
+        novaBolhaBot.innerHTML = resposta;
+        chat.appendChild(novaBolhaBot);
+
+        vaiParaFinalDoChat();
+    }
+
+    function obterInformacoesDoTemaSimulado(tema) {
+        const dadosSimulados = {
+            temas: {
+                "saudacoes": {
+                    "como dizer olá em diferentes idiomas": "Olá! em inglês é Hello, em espanhol é Hola, em francês é Bonjour, em alemão é Hallo, e assim por diante.",
+                    "oi": "Oi! Como posso ajudar hoje?",
+                    // ... Outras respostas ...
                 },
-                body: JSON.stringify({ 'msg': mensagem }),
-            });
+                // Adicione outros temas conforme necessário
+            },
+            respostas_simuladas: [
+                "Não entendi, pode reformular a pergunta?",
+                "Desculpe, não posso responder a isso no momento.",
+                // ... Outras respostas simuladas ...
+            ],
+        };
 
-            const decodificador = new TextDecoder();
-            const leitorDaResposta = resposta.body.getReader();
-            let respostaParcial = "";
+        const informacoes = dadosSimulados.temas[tema];
 
-            while (true) {
-                const { done: terminou, value: pedacoDaResposta } = await leitorDaResposta.read();
-                if (terminou) break;
-
-                respostaParcial += decodificador.decode(pedacoDaResposta).replace(/\n/g, '<br>');
-
-                const novaBolhaBotParte = criaBolhaBot();
-                novaBolhaBotParte.innerHTML = respostaParcial;
-                chat.appendChild(novaBolhaBotParte);
-            }
-
-            vaiParaFinalDoChat();
-        } catch (erro) {
-            console.error("Erro na requisição:", erro);
+        if (informacoes) {
+            return informacoes.join('<br>');
+        } else {
+            const respostasSimuladas = dadosSimulados.respostas_simuladas;
+            return respostasSimuladas[Math.floor(Math.random() * respostasSimuladas.length)];
         }
     }
-}
 
-// Restante do código ...
+    function criaBolhaBot() {
+        const bolhaBot = document.createElement('p');
+        bolhaBot.classList = 'chat__bolha chat__bolha--bot';
+        return bolhaBot;
+    }
 
-function vaiParaFinalDoChat() {
-    chat.scrollTop = chat.scrollHeight;
-}
+    function criaBolhaUsuario() {
+        const bolhaUsuario = document.createElement('p');
+        bolhaUsuario.classList = 'chat__bolha chat__bolha--usuario';
+        return bolhaUsuario;
+    }
+
+    function vaiParaFinalDoChat() {
+        chat.scrollTop = chat.scrollHeight;
+    }
+});
+// let chat = document.querySelector('#chat');
+// let input = document.querySelector('#input');
+// let botaoEnviar = document.querySelector('#botao-enviar');
+
+// async function enviarMensagem() {
+//     if (!input.value.trim()) return;
+
+//     const mensagem = input.value.trim();
+//     input.value = "";
+
+//     const novaBolhaUsuario = criaBolhaUsuario();
+//     novaBolhaUsuario.textContent = mensagem;
+//     chat.appendChild(novaBolhaUsuario);
+
+//     const novaBolhaBot = criaBolhaBot();
+//     chat.appendChild(novaBolhaBot);
+//     vaiParaFinalDoChat();
+
+//     // Simulação da resposta da API localmente
+//     if (window.location.hostname === "localhost") {
+//         setTimeout(() => {
+//             const respostaSimulada = "Resposta simulada do ChatBot para: " + mensagem;
+//             const novaBolhaBotParte = criaBolhaBot();
+//             novaBolhaBotParte.innerHTML = respostaSimulada;
+//             chat.appendChild(novaBolhaBotParte);
+//             vaiParaFinalDoChat();
+//         }, 50000); // Simula um atraso de 1 segundo
+//     } else {
+//         // Envia requisição com a mensagem para a API do ChatBot
+//         try {
+//             const resposta = await fetch("http://127.0.0.1:5000/chat", {
+//                 method: "POST",
+//                 headers: {
+//                     "Content-Type": "application/json",
+//                 },
+//                 body: JSON.stringify({ 'msg': mensagem }),
+//             });
+
+//             const decodificador = new TextDecoder();
+//             const leitorDaResposta = resposta.body.getReader();
+//             let respostaParcial = "";
+
+//             while (true) {
+//                 const { done: terminou, value: pedacoDaResposta } = await leitorDaResposta.read();
+//                 if (terminou) break;
+
+//                 respostaParcial += decodificador.decode(pedacoDaResposta).replace(/\n/g, '<br>');
+
+//                 const novaBolhaBotParte = criaBolhaBot();
+//                 novaBolhaBotParte.innerHTML = respostaParcial;
+//                 chat.appendChild(novaBolhaBotParte);
+//             }
+
+//             vaiParaFinalDoChat();
+//         } catch (erro) {
+//             console.error("Erro na requisição:", erro);
+//         }
+//     }
+// }
+
+// // Restante do código ...
+
+// function vaiParaFinalDoChat() {
+//     chat.scrollTop = chat.scrollHeight;
+// }
